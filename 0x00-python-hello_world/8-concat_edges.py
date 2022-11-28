652276ad992ec3181c70232c1bbddf6dbafe7223
larsensong <larsenmulamba@gmail.com>
2022-11-28 19:14:34 +0000

    task8

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "python is a interactive, interpreted and an oblect-oriented programming\
+language that combines power using clear and well format"
+str = str[39:66] + str[106:112] + str[:6]
+print(str)
