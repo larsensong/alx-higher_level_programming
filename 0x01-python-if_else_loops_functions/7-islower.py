68e58eb479a0cbee40a7b546c677ea4e316fdedf
larsensong <larsenmulamba@gmail.com>
2022-11-30 15:59:09 +0000

    checks for lowercase character

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+#larsen song
+
+
+
+def islower(c):
+
+    """Function checks for lowercase characters."""
+
+    if ord(c) >= 97 and ord(c) <= 122:
+
+        return True
+
+    else:
+
+        return False
