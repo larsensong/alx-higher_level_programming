ef857917e761845621a2c0e0739a80131545f67f
larsensong <larsenmulamba@gmail.com>
2022-11-30 17:05:26 +0000

    fixing error

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+
+{
+
+	listint_t *slow = list;
+
+	listint_t *fast = list;
+
+
+
+	if (!list)
+
+		return (0);
+
+
+
+	while (slow && fast && fast->next)
+
+	{
+
+		slow = slow->next;
+
+		fast = fast->next->next;
+
+		if (slow == fast)
+
+			return (1);
+
+	}
+
+
+
+	return (0);
+
+}
