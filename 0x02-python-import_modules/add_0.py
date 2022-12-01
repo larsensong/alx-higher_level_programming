061db659416bdd15f43fc0cd758d5e5670bf0b88
larsensong <larsenmulamba@gmail.com>
2022-12-01 06:12:19 +0000

    program that imports the function and prints results

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def add(a, b):
+
+    """ The addition function
+
+    Arguments:
+
+        a: first integer
+
+        b: second integer
+
+    Returns:
+
+        The return value. a + b
+
+    """
+
+    return (a + b)
