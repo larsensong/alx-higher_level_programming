d40943b03f8d154674604a7d33a4a76fd243f226
larsensong <larsenmulamba@gmail.com>
2022-12-07 17:44:01 +0000

    function that computes the square value of all integers

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+
+    return [list(map(lambda n: n**2, sublist)) for sublist in matrix]
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+My data structures and set dictionary
