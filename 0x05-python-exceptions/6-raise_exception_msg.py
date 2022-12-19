2d943d66cd717746dea60e1726027ed96b7889b0
larsensong <larsenmulamba@gmail.com>
2022-12-19 06:59:50 +0000

    function that raises a name exception with a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# 6-raise_exception_msg.py
+
+# Author Larsen song
+
+
+
+
+
+def raise_exception_msg(message=""):
+
+    """Raise a NameError exception with a message."""
+
+    raise NameError(message)
