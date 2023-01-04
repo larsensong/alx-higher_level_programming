ee000c46af373d360d1a73f26060bf11c9689a13
larsensong <larsenmulamba@gmail.com>
2023-01-04 20:13:29 +0000

    pythonic and memory test

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# 101-locked_class.py
+
+#Larsen song
+
+"""Defines a locked class."""
+
+
+
+
+
+class LockedClass:
+
+    """
+
+    Prevent the user from instantiating new LockedClass attributes
+
+    for anything but attributes called 'first_name'.
+
+    """
+
+
+
+    __slots__ = ["first_name"]
