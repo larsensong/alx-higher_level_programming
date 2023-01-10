661778c356b323f1c6f39544f9edb91f48509c90
larsensong <larsenmulamba@gmail.com>
2023-01-10 19:23:23 +0000

    fix errors

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-# 7-add_item.py
-
+# 9-add_item.py
 
 
 """Add all arguments to a Python list and save them to a file."""
@@ -12,11 +11,11 @@ import sys
 
 if __name__ == "__main__":
 
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
     load_from_json_file = \
 
-        __import__('6-load_from_json_file').load_from_json_file
+        __import__('8-load_from_json_file').load_from_json_file
 
 
 
