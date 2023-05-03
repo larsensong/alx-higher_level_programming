5b4515c65374057456b3d6965f54bd3247864bda
root <larsenmulamba@gmail.com>
2023-05-03 20:41:52 +0000

    update

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+const $ = window.$;
+$('DIV#toggle_header').click(function () {
+  $('header').toggleClass('red');
+  if ($('header.red')) {
+    $('header').toggleClass('green');
+  } else {
+    $('header').toggleClass('red');
+  }
+});
